9c8ec85cfec1977aa5ffac8459b27ba20de7abc7
Lyndsey Browning <lbrowning86@gmail.com>
2016-12-28 15:22:11 +0000

    Added Webpack

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var webpack = require('webpack');
+var WebpackNotifierPlugin = require('webpack-notifier');
+
+/*
+ * Default webpack configuration for development
+ */
+var config = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/src/index.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      }
+    ]
+  },
+
+  postcss: [
+    require('autoprefixer')
+  ],
+
+  plugins: [
+    new WebpackNotifierPlugin()
+  ],
+
+  devServer: {
+    colors: true,
+    historyApiFallback: true,
+    post: process.env.PORT || 8080,
+    inline: true
+  },
+}
+
+module.exports = config;

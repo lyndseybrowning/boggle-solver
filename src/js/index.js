3d205c3b085855702ee4307af44c801b536b5618
Lyndsey Browning <lbrowning86@gmail.com>
2016-12-29 11:26:12 +0000

    Updated dictionary URL

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ import css from '../css/main.scss';
 import solve from './solver.js';
 import 'whatwg-fetch';
 
-fetch('../../dictionary/sowpods.json')
+fetch('/dictionary/sowpods.json')
 .then(response => { return response.json() })
 .then(body => { solve(body) });

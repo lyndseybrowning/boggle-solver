c74bc800a913e85a31a58011c1313152c71dc563
Lyndsey Browning <lbrowning86@gmail.com>
2016-12-29 17:46:52 +0000

    Updated default 6x6 letters

--- src/js/settings.js
+++ src/js/settings.js
@@ -4,7 +4,7 @@ export default  {
     3: 'ser lin ers',
     4: 'sers patg line sers',
     5: 'rscls deiae gntrp iaeso lmidn',
-    6: 'dsrodg temends rasito dgntrp reiaes tsclpd',
+    6: 'dsrodg temens rasito dgntrp reiaes tsclpd',
     7: 'serafki ngejake nmetuip mmserop smaleid dkslaen msoeiuw',
     8: 'sersinel estlrops ingdlobd sersline dnockuia nonplesr sseloutc slemnthd'
   },

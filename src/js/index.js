a5b76008f92597abb221d592d22b8afdbdbf46d8
Lyndsey Browning <lbrowning86@gmail.com>
2016-12-29 12:48:35 +0000

    build

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import css from '../css/main.scss';
 import solve from './solver.js';
 import 'whatwg-fetch';
 
-const env = process.env.NODE_ENV || 'production';
+const env = process.env.NODE_ENV || 'development';
 let path = '/dictionary';
 if(env === 'production') {
   path = 'lyndseyb.co.uk/boggle-solver-js/dictionary';

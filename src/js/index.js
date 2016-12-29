907286c4e1b4eb66c15dc50da5a10b67d909af0f
Lyndsey Browning <lbrowning86@gmail.com>
2016-12-29 12:49:45 +0000

    build

--- src/js/index.js
+++ src/js/index.js
@@ -2,10 +2,10 @@ import css from '../css/main.scss';
 import solve from './solver.js';
 import 'whatwg-fetch';
 
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 let path = '/dictionary';
 if(env === 'production') {
-  path = 'lyndseyb.co.uk/boggle-solver-js/dictionary';
+  path = '/boggle-solver-js/dictionary';
 }
 
 fetch(`${path}/sowpods.json`)
